84a7dc2c5dd1469cbce9d300133bd06c27f4f9cf
sergei <sergei@sdfsfrse2435359>
2016-08-07 17:44:32 +0300

    fin less

--- less_3_Method/src/less_3_Method/Main.java
+++ less_3_Method/src/less_3_Method/Main.java
@@ -3,8 +3,19 @@ package less_3_Method;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Просто пример создания метода
+		int i = 3;
+		int k = 5;
+		i = count(i);
+		k = count(k);
+		System.out.println(i);
+		System.out.println(k);
 
+	}
+
+	private static int count(int x) {
+		x = x + 1;
+		return x;
 	}
 
 }
