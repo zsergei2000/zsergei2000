4a2dc250a96eb85afdb0165e95d7f026b2013eb7
sergei <sergei@sdfsfrse2435359>
2016-08-07 15:00:12 +0300

    1

--- Gid_test_pr/src/GT_Test/Main.java
+++ Gid_test_pr/src/GT_Test/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("GH");
+		System.out.println("GH"); //
+		//new str
 
 	}
 
