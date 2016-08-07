939650f074617dd26eeedd868bb6db5595f20ab5
sergei <sergei@sdfsfrse2435359>
2016-08-07 21:45:18 +0300

    1

---
+++ less_9_Massiv/src/less_9_Massiv/Main_Vivod_Massiva.java
@@ -0,0 +1,15 @@
+package less_9_Massiv;
+
+public class Main_Vivod_Massiva {
+
+	public static void main(String[] args) {
+		int[] m = new int[5];
+		
+		for(int x=0; x<5; x++){
+			m[x] = x;
+			System.out.println(m[x]);
+		}
+
+	}
+
+}
